eb405656ca5a9714924157c60bd1a7bc2937e4fa
Robert Lankford <robbylankford@gmail.com>
2022-11-19 16:06:13 -0500

    demonstrate do while loops

---
+++ harvard/cs50/01_c/mario.c
@@ -0,0 +1,38 @@
+#include ".vscode/cs50.h"
+#include <stdio.h>
+
+int get_height(void);
+int get_width(void);
+
+int main(void) {
+    // Get positive integer height from user
+    int height = get_height();
+    int width = get_width();
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+int get_height(void) {
+    int n;
+    do 
+    {
+        n = get_int("Height: ");
+    }
+    while(n < 1);
+    return n;
+}
+
+int get_width(void) {
+    int n;
+    do 
+    {
+        n = get_int("Width: ");
+    }
+    while(n < 1);
+    return n;
+}
