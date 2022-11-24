8d0317c65ce3594aa466a819d7622a602872fa34
Robert Lankford <robbylankford@gmail.com>
2022-11-24 09:33:17 -0500

    program to demo exit statuses

---
+++ harvard/cs50/02_arrays/exit.c
@@ -0,0 +1,16 @@
+#include ".vscode/cs50.h"
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Missing command-line arguments\n");
+        return 1;
+    }
+
+    printf("hello, %s\n", argv[1]);
+    return 0;
+}
+
+// run echo $? in terminal after running program to see exit status
