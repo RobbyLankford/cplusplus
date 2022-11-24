a7f93c2918bb15938a06353f2e228ba6a172dde3
Robert Lankford <robbylankford@gmail.com>
2022-11-24 09:33:03 -0500

    program to demo command-line arguments

---
+++ harvard/cs50/02_arrays/argv.c
@@ -0,0 +1,20 @@
+#include ".vscode/cs50.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[]) 
+{
+    if (argc == 2) 
+    {
+        printf("hello, %s\n", argv[1]);
+        
+        for (int i = 0; i < strlen(argv[1]); i++)
+        {
+            printf("%c\n", argv[1][i]);
+        }
+    }
+    else
+    {
+        printf("hello, world\n");
+    }
+}
