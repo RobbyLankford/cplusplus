e7fa0c19b743b3dc2de7fe5a0cebe2751ab3368c
Robert Lankford <robbylankford@gmail.com>
2022-11-26 13:02:57 -0500

    program to demonstrate using struct

---
+++ harvard/cs50/03_algorithms/phonebook.c
@@ -0,0 +1,35 @@
+#include ".vscode/cs50.h"
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    string name;
+    string number;
+}
+person; 
+
+const int NUMBER = 10;
+
+int main(void)
+{
+    person people[NUMBER];
+
+    for (int i = 0; i < NUMBER; i++)
+    {
+        people[i].name = get_string("What's the name? ");
+        people[i].number = get_string("What's their number? ");
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(people[i].name, "David") == 0)
+        {
+            printf("Found %s\n", people[i].number);
+            return 0;
+        }
+    }
+    
+    printf("Not Found\n");
+    return 1;
+}
