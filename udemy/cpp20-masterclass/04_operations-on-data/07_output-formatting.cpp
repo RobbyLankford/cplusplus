6f148a1dfe8c74cde1a40b53de322889455ea60a
Robert Lankford <robbylankford@gmail.com>
2023-01-04 15:53:55 -0500

    outline for program on output formatting

---
+++ udemy/cpp20-masterclass/04_operations-on-data/07_output-formatting.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip>
+#include <ios>
+#include <ostream>
+
+int main()
+{
+    // std::endl : places a new line '\n' character on the output stream
+
+
+    // std::flush : forces output to the terminal at the point this command is called
+
+
+    // std::setw : sets the width of the output for nice formatting
+
+
+    // Justification of text: std::right, std::left, or std::internal
+
+
+    // std::setfill : sets the character used in empty spaces
+
+
+    // std::boolalpha and std::noboolalpha : controls if bools are printed as true/false or 1/0
+
+    
+    // std::showpos and std::noshowpos : controls if `+` is shown for positive numbers
+
+
+    // std::dec, std::hex, and std::oct : controls the base numbering systems used
+
+
+    // std::uppercase : force output letters to uppercase
+
+
+    // std::fixed and std::scientific : controls how decimals are printed
+
+
+    // A hacky way to set back to the default way of printing floating points
+
+
+    // std::setprecision : sets precision with which decimals are printed
+
+
+    // std::showpoint : forces trailing zeros to be shown
+
+
+    return 0;
+}
