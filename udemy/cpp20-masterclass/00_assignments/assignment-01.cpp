3495b74aa4776af66006ccdfb335b1e158718fc0
Robert Lankford <robbylankford@gmail.com>
2023-01-11 09:58:55 -0500

    move assignment 1 to assignment directory

--- udemy/cpp20-masterclass/00_assignments/assignment-01.cpp
+++ udemy/cpp20-masterclass/00_assignments/assignment-01.cpp
@@ -3,11 +3,14 @@
 
 int main() 
 {
+    // Declare variables
     std::string response;
     
+    // Gather information from user
     std::cout << "Where do you live? ";
     std::getline(std::cin, response);
 
+    // Respond to user
     std::cout << "I've heard great things about " << response << ". I'd like to go sometime." << std::endl;
 
     return 0;
