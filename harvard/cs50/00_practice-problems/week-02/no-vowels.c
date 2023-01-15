8ac9e95b8d090b05576534f1a40b9cfdfeef88d6
Robert Lankford <robbylankford@gmail.com>
2023-01-14 19:19:52 -0500

    implement no-vowels program

---
+++ harvard/cs50/00_practice-problems/week-02/no-vowels.c
@@ -0,0 +1,55 @@
+// Write a function to replace vowels with numbers
+// Get practice with strings
+// Get practice with command line
+// Get practice with switch
+
+#include ".vscode/cs50.h"
+#include <stdio.h>
+#include <string.h>
+
+string replace(string word);
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./novowels word\n");
+        return 1;
+    }
+
+    string new_word = replace(argv[1]);
+    printf("%s\n", new_word);
+
+    return 0;
+}
+
+// Replace the characters
+string replace(string word)
+{
+    for (int i = 0; i < strlen(word); i++)
+    {
+        switch (word[i])
+        {
+            case 'a':
+                word[i] = '6';
+                break;
+
+            case 'e':
+                word[i] = '3';
+                break;
+
+            case 'i':
+                word[i] = '1';
+                break;
+
+            case 'o':
+                word[i] = '0';
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    return word;
+}
