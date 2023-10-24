4c138c569bab0660d075a838a4ff17aef9edbf77
Robert Lankford <robbylankford@gmail.com>
2023-10-23 20:16:50 -0400

    section 2.7

---
+++ the-c-programming-language/02_types-operators-expressions/2.07/ex-02.c
@@ -0,0 +1,17 @@
+/* lower: convert c to lower case; ASCII only */
+int lower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return c + 'a' - 'A';
+    }
+    else
+    {
+        return c;
+    }
+}
+
+main()
+{
+    return 0;
+}
