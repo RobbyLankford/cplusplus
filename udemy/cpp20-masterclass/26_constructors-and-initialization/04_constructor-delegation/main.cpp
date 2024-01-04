2441fc7b631b1a979e6e26a188543e08204097e3
Robert Lankford <robbylankford@gmail.com>
2024-01-03 19:39:06 -0500

    program to demo constructor delegation

---
+++ udemy/cpp20-masterclass/26_constructors-and-initialization/04_constructor-delegation/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "square.h"
+
+int main()
+{
+    // Creating a square object with one parameter will call the three-parameter constructor
+    Square s1(100.0);
+
+    return 0;
+}
