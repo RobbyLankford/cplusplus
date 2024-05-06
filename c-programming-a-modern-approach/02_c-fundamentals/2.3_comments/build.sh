a5141a83553dc13bbe7df7e6affeb364d8c81ec2
Robert Lankford <robbylankford@gmail.com>
2024-05-06 13:41:05 -0400

    section 2.3

---
+++ c-programming-a-modern-approach/02_c-fundamentals/2.3_comments/build.sh
@@ -0,0 +1,3 @@
+mkdir -p build
+gcc -o build/pun pun.c
+build/pun
---
+++ c-programming-a-modern-approach/02_c-fundamentals/2.3_comments/pun.c
@@ -0,0 +1,51 @@
+/* 
+    Recommended that each program have comments containing:
+    1. Program Name
+    2. Date Written
+    3. Author
+    4. Program Purpose
+*/
+
+/* Method 1 */
+/* Name: pun.c */
+/* Purpose: Prints a bad pun */
+/* Author: K. N. King */
+
+/* Method 2 */
+/* Name: pun.c
+   Purpose: Prints a bad pun
+   Author: K. N. King */
+
+/* Method 3 */
+/* Name: pun.c 
+   Purpose: Prints a bad pun
+   Author: K. N. King
+*/
+
+/* Method 4 */
+/****************************
+* Name: pun.c               *
+* Purpose: Prints a bad pun *
+* Author: K. N. King        *
+*****************************/
+
+/* Method 5 */
+/*
+* Name: pun.c
+* Purpose: Prints a bad pun
+* Author: K. N. King
+*/
+
+/* Method 6 (C99) */
+// Name: pun.c
+// Purpose: Prints a bad pun
+// Author: K. N. King
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("To C, or not to C: that is the question.\n");
+
+    return 0;
+}
