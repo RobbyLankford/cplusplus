decfa197f4d23cc4e1d7ddcdc14fcf63a613828b
Robert Lankford <robbylankford@gmail.com>
2024-05-14 17:40:46 -0400

    chapter 2 exercises

---
+++ c-programming-a-modern-approach/02_c-fundamentals/exercises/ex-04/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int int1, int2, int3;
+    float float1, float2, float3;
+
+    printf("int1: %d\n", int1);
+    printf("int2: %d\n", int2);
+    printf("int3: %d\n", int3);
+
+    printf("\n");
+
+    printf("float1: %f\n", float1);
+    printf("float2: %f\n", float2);
+    printf("float3: %f\n", float3);
+
+    return 0;
+}
---
+++ c-programming-a-modern-approach/02_c-fundamentals/exercises/ex-09/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int p, q;
+    float answer;
+
+    answer = (3 * q - p * p) / 3.0;
+
+    return 0;
+}
