de76fbbc3caf66f46d00aecd535362d284247f87
Robert Lankford <robbylankford@gmail.com>
2024-06-22 14:08:14 -0400

    simple bash script to automate file creation

---
+++ udemy/cpp20-masterclass/create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# create new chapter and section folders
+path="$1"
+tail="${path##*/}"
+head="${path%/*}"
+
+mkdir -p $head
+mkdir -p $path
+
+# create main.cpp file
+cat > $path/main.cpp << EOL
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
+EOL
+
+# create build script
+cat > $path/build.sh << EOL
+mkdir -p build
+g++ -o build/main main.cpp
+build/main
+EOL
+
+open $path/main.cpp
