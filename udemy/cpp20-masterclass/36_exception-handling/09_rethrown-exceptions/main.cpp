40238074c187788b39f323cb118351d91a0df16f
Robert Lankford <robbylankford@gmail.com>
2024-06-24 11:58:25 -0400

    program to demo rethrown exceptions

---
+++ udemy/cpp20-masterclass/36_exception-handling/09_rethrown-exceptions/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#include "exceptions.h"
+
+void do_something(size_t i)
+{
+    if (i == 2)
+        throw CriticalError("i is 2");
+    
+    if (i == 3)
+        throw SmallError("i is 3");
+    
+    if (i == 4)
+        throw Warning("i is 4");
+    
+    std::cout << "Doing something at iteration: " << i << std::endl;
+}
+
+int main()
+{
+    for (size_t i {0}; i < 5; i++)
+    {
+        try
+        {
+            try
+            {
+                do_something(i);
+            }
+            catch (SomethingIsWrong &ex_inner)
+            {
+                if (typeid(ex_inner) == typeid(Warning))
+                {
+                    std::cout << typeid(ex_inner).name() 
+                              << "-Inner catch block, Exception caught: " 
+                              << ex_inner.what() << std::endl;
+                } else
+                {
+                    throw;
+                }
+            }
+        }
+        catch (SomethingIsWrong &ex_outer)
+        {
+            std::cout << typeid(ex_outer).name()
+                      << "-Outer catch block, Exception caught: "
+                      << ex_outer.what() << std::endl;
+        }
+    }
+
+    return 0;
+}
