5d7cc0555cb52b40d4d6d360dc8beac98b5981a5
Robert Lankford <robbylankford@gmail.com>
2024-11-20 11:41:50 -0500

    program to demo constant iterators

---
+++ udemy/cpp20-masterclass/45_custom-iterators/08_constant-iterators/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <ranges>
+
+#include "boxcontainer.h"
+
+template <typename T>
+void print(const BoxContainer<T> &c)
+{
+    for (auto i : c)
+        std::cout << i << " ";
+    std::cout << std::endl;
+}
+
+int main()
+{
+    BoxContainer<int> vi;
+    vi.add(5);
+    vi.add(1);
+    vi.add(7);
+    vi.add(2);
+    vi.add(5);
+    vi.add(3);
+    vi.add(7);
+    vi.add(9);
+    vi.add(6);
+
+    const BoxContainer<int> copy(vi);
+
+    std::cout << "data: ";
+    for (auto it = copy.begin(); it != copy.end(); it++)
+        std::cout << (*it) << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
