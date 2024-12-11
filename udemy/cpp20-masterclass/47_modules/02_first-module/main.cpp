5dac695380058219e8190e88f2a5173f9a00aab8
Robert Lankford <robbylankford@gmail.com>
2024-12-11 17:32:46 -0500

    program to demo a simple module

---
+++ udemy/cpp20-masterclass/47_modules/02_first-module/main.cpp
@@ -0,0 +1,14 @@
+import <iostream>;
+import math_stuff;
+
+int main()
+{
+    auto result = add(10, 20);
+
+    std::cout << "Result: " << result << std::endl;
+
+    greet("John");
+    print_name_length("John");
+
+    return 0;
+}
