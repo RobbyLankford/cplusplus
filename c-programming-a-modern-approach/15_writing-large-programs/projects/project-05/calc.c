bb9c56dd30902d015d49144e8bf460b44fda1c48
Robert Lankford <robbylankford@gmail.com>
2024-12-13 11:14:38 -0500

    chapter 15 projects

---
+++ c-programming-a-modern-approach/15_writing-large-programs/projects/project-05/calc.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#include "stack.h"
+
+int main(void)
+{
+    char c, op1, op2;
+
+    while (true)
+    {
+        printf("Enter an RPN expression: ");
+        while ((c = getchar()) != '\n')
+        {
+            if (isdigit(c))
+                push(c - '0');
+            else
+            {
+                switch(c)
+                {
+                    case '+': 
+                        push(pop() + pop());
+
+                        break;
+
+                    case '-':
+                        op2 = pop();
+                        op1 = pop();
+                        push(op1 - op2);
+
+                        break;
+                    
+                    case '*':
+                        push(pop() * pop());
+
+                        break;
+                    
+                    case '/':
+                        op2 = pop();
+                        op1 = pop();
+                        push(op1 / op2);
+
+                        break;
+                    
+                    case '=':
+                        printf("Value of expression: %d\n", pop());
+
+                        break;
+                    
+                    case ' ':
+                        break;
+                    
+                    default:
+                        exit(EXIT_FAILURE);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ c-programming-a-modern-approach/15_writing-large-programs/projects/project-05/stack.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "stack.h"
+
+#define STACK_SIZE 100
+
+char contents[STACK_SIZE] = {0};
+int top = 0;
+
+void make_empty(void)
+{
+    top = 0;
+}
+
+bool is_empty(void)
+{
+    return top == 0;
+}
+
+bool is_full(void)
+{
+    return top == STACK_SIZE;
+}
+
+void push(char c)
+{
+    if (is_full())
+        stack_overflow();
+    else
+        contents[top++] = c;
+}
+
+char pop(void)
+{
+    if (is_empty())
+        stack_underflow();
+    else
+        return contents[--top];
+}
+
+void stack_overflow(void)
+{
+    printf("\nExpression is too complex.\n");
+    exit(EXIT_FAILURE);
+}
+
+void stack_underflow(void)
+{
+    printf("\nNot enough operands in expression.\n");
+    exit(EXIT_FAILURE);
+}
